2d11b4014905bea0820e6914c40286412aca3105
JoshDesmond <JoshDesmond@users.noreply.github.com>
2020-07-29 13:31:04 -0400

    Added stub MVC classes to index.js

--- www/src/index.js
+++ www/src/index.js
@@ -1,1 +1,20 @@
 console.log("Hello");
+
+class Model {
+    constructor() {
+        console.log("World.");
+    }
+}
+  
+class View {
+    constructor() {}
+}
+  
+class Controller {
+    constructor(model, view) {
+        this.model = model
+        this.view = view
+    }
+}
+
+const app = new Controller(new Model(), new View());
