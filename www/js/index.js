781d2a563d2d338444acbde71b047e44da6eba1b
Josh Desmond <JoshDesmond@users.noreply.github.com>
2021-09-09 22:27:20 -0400

    Development (#6)
    * Begins development of html content
    
    * Adds general html skeleton
    
    * Adds footer links & copyright
    
    * Adds about me text content
    
    * Adds skeleton.css
    
    * Begins work on styling content, adds fonts
    
    * Styles the intro and about sections
    
    * Styles intro and about some more
    
    * Adds html canvas to intro
    
    * Adds preliminary content for portfolio and contact
    
    * Styles and adds content for portfolio
    
    * Starts progress on styling canvas & navigation
    
    * Begins work on canvas animations
    
    * Finishes basic work on canvas particles
    
    * Begins styling on nav bar
    
    * Tweaks some particle parameters
    
    * Adds footer styling

--- www/js/index.js
+++ www/js/index.js
@@ -1,14 +1,45 @@
-import {Model} from "./model/model.js";
-import {View} from "./views/views.js";
+import { Model } from "./model/model.js";
+import { View } from "./views/views.js";
 
 console.log("Hello");
 
 
-class Controller {
+class App {
+    /**
+     * 
+     * @param {Model} model 
+     * @param {View} view 
+     */
     constructor(model, view) {
-        this.model = model
-        this.view = view
+        this.model = model;
+        this.view = view;
+
+        this.view.model = model;
+
+        for (const p of this.model.particles) {
+            p.subscribeObserver(this.view);
+        }
+
+
+        canvas.addEventListener('mousemove', (event) => {
+            this.model.updateMousePosition(event.x, event.y);
+        });
+
+        const resizeObserver = new ResizeObserver(entries => {
+            for (let entry of entries) {
+                model.updateCanvasSize(entry.contentRect.width, entry.contentRect.height);
+                view.resetCanvasSize();
+            }
+        });
+
+        resizeObserver.observe(canvas);
+
+        this.view.animate();
     }
 }
 
-const app = new Controller(new Model(), new View());
+
+const views = new View(document);
+const canvas = document.getElementById('intro-canvas');
+const model = new Model(canvas.getBoundingClientRect().width, canvas.getBoundingClientRect().height);
+const app = new App(model, views);
--- www/js/model/model.js
+++ www/js/model/model.js
@@ -1,6 +1,113 @@
 export class Model {
-    constructor() {
-        console.log("World.");
+    constructor(width, height) {
+        this.canvasHeight = height;
+        this.canvasWidth = width;
+        this.initializeParticles();
+    }
+
+    initializeParticles() {
+        this.particles = [];
+        const numberOfParticles = (this.canvasHeight * this.canvasWidth) / 9000;
+        for (let i = 0; i < numberOfParticles; i++) {
+            const size = (Math.random() * 5) + 1;
+            const x = (Math.random() * ((this.canvasWidth - size * 2) - (size * 2)) + size * 2);
+            const y = (Math.random() * ((this.canvasHeight - size * 2) - (size * 2)) + size * 2);
+            const directionX = (Math.random() * 4) - 2.5;
+            const directionY = (Math.random() * 4) - 2.5;
+            const color = '#132620';
+
+            this.particles.push(new Particle(x, y, directionX, directionY, size, color));
+        }
+    }
+
+    tick() {
+        for (const p of this.particles) {
+            this.updateParticle(p);
+        }
+    }
+
+    updateMousePosition(x, y) {
+        this.mouseX = x;
+        this.mouseY = y;
+        this.mouseRadius = (this.canvasHeight / 100) * (this.canvasWidth / 100);
+    }
+
+    updateCanvasSize(width, height) {
+        this.canvasWidth = width;
+        this.canvasHeight = height;
+    }
+
+    updateParticle(particle) {
+        // Wall bounces
+        if (particle.x > this.canvasWidth || particle.x < 0) {
+            particle.directionX = -particle.directionX;
+        }
+        if (particle.y > this.canvasHeight || particle.y < 0) {
+            particle.directionY = -particle.directionY;
+        }
+
+        const collisionFunction = (x, y, radius, speed) => {
+            const dx = x - particle.x;
+            const dy = y - particle.y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+            if (distance < radius + particle.size) {
+                if (x < particle.x && particle.x < this.canvasWidth - particle.size * 10) {
+                    particle.directionX += speed;
+                }
+                if (x > particle.x && particle.x > particle.size * 10) {
+                    particle.directionX -= speed;
+                }
+
+                if (y < particle.y && particle.y < this.canvasHeight - particle.size * 10) {
+                    particle.directionY += speed;
+                }
+                if (y > particle.y && particle.y > particle.size * 10) {
+                    particle.directionY -= speed;
+                }
+            }
+        }
+
+        // Mouse collision
+        collisionFunction(this.mouseX, this.mouseY, this.mouseRadius, .2);
+        // Collision bubble around center of screen
+        collisionFunction(this.canvasWidth/2, this.canvasHeight/2, this.canvasWidth/6, .05);
+
+        // Move particles
+        particle.x = particle.x + particle.directionX;
+        particle.y = particle.y + particle.directionY;
+        if (particle.directionX > 5) {
+            particle.directionX -= .2;
+        }
+        if (particle.directionY > 5) {
+            particle.directionY -= .2;
+        }
+        if (particle.directionX < -5) {
+            particle.directionX += .2;
+        }
+        if (particle.directionY < -5) {
+            particle.directionY += .2;
+        }
+        particle.update();
+    }
+
+
+}
+
+export class Particle {
+    constructor(x, y, directionX, directionY, size, color) {
+        this.x = x;
+        this.y = y;
+        this.directionX = directionX;
+        this.directionY = directionY;
+        this.size = size;
+        this.color = color;
+    }
+
+    subscribeObserver(observer) {
+        this.observer = observer;
+    }
+
+    update() {
+        this.observer.notify(this);
     }
 }
- 
